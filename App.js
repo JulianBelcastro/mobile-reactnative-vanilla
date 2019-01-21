ae6eb45454fdbe357e929f3f4fb05e7dd5247aab
Julian Belcastro <julian.belcastro@gmail.com>
2019-01-21 14:01:25 +1100

    firstName/lastName/submitButton placed.

--- App.js
+++ App.js
@@ -7,43 +7,54 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Button} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+import t from 'tcomb-form-native';
+
+const Form = t.form.Form;
+
+const User = t.struct({
+    firstName:t.String,
+    lastName: t.String,
 });
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+const Options = {
+    order:[ 'firstName', 'lastName'],
+    fields:{
+        firstName: {
+        placeholder: 'Please enter a firstName',
+        },
+        lastName: {
+        placeholder: 'Please enter a lastName'
+        },
+
+    }
+}
+
+export default class App extends Component{
+    handleSubmit = () => {
+    };
+    render(){
+        return(
+        <View style={styles.container}>
+        <Form
+            type={User}
+            options={Options}
+          />
+        <Button
+            title="Submit"
+            onPress={this.handleSubmit}
+          />
+        </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    marginTop: 50,
+    padding: 20,
+    backgroundColor: '#ffffff',
   },
 });
